8e0e109831dcf979df85ce0dd4f9b61c67e00e87
ArkadiuszGalat <sotalag@gmail.com>
2021-12-18 11:21:16 +0100

    z warunkami

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #%%
 # gra w kółko i krzyżek na 3 
 #poszukać biblioteki pretty console
+#https://github.com/angelm1974/kolko
 plansza_do_gry = {'7':' ', '8':' ', '9':' ', '4':' ', '5':' ', '6':' ', '1':' ', '2':' ', '3':' ', }
 print(plansza_do_gry)
 print(f'')
@@ -15,4 +16,61 @@ def drukuj_plansze(pole):
     print('---+---+---')
     print(f" {pole['1']} | {pole['2']} | {pole['3']}")
 #komentarz
-drukuj_plansze(plansza_do_gry)
+#drukuj_plansze(plansza_do_gry)
+def gra():
+    gracz='X'
+    ruch_licznik=0
+    for i in range(10):
+        drukuj_plansze(plansza_do_gry)
+        print(f'To jest ruch, gracza {gracz}. Wybierz z klawiatury num gdzie chcesz postawić znak!')
+        
+        ruch=input()
+
+        if plansza_do_gry[ruch]==' ':
+            plansza_do_gry[move]=gracz
+            ruch+=1
+        else:
+            print('To pole jest zajęte!\n')
+            continue
+        if ruch_licznik>=5:
+            if plansza_do_gry['7']==plansza_do_gry['8']==plansza_do_gry['9']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+            elif plansza_do_gry['4']==plansza_do_gry['5']==plansza_do_gry['6']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+            elif plansza_do_gry['1']==plansza_do_gry['2']==plansza_do_gry['3']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+            elif plansza_do_gry['1']==plansza_do_gry['5']==plansza_do_gry['9']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+            elif plansza_do_gry['3']==plansza_do_gry['5']==plansza_do_gry['7']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+            elif plansza_do_gry['1']==plansza_do_gry['4']==plansza_do_gry['7']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+            elif plansza_do_gry['2']==plansza_do_gry['5']==plansza_do_gry['8']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+            elif plansza_do_gry['3']==plansza_do_gry['6']==plansza_do_gry['9']!=' ':
+                drukuj_plansze(plansza_do_gry)
+                print('KONIEC!')
+                print(f'Wygrał gracz: {gracz}')
+                break
+              
